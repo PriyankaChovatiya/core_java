4641136a255fc9d317e7cfad144a4c654c49f41c
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-23 15:14:03 +0530

    If statement

---
+++ src/IfExample.java
@@ -0,0 +1,11 @@
+class IfExample {
+
+    public static void main(String arg[]){
+        int n = 10;
+        if(n>0){
+            //If body: if this condition true then print this statement
+           System.out.println("Yes it is grater then " +n);
+
+        }
+    }
+}
