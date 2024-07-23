9ba567d727618a89e477705682d3dac6d06474a8
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-23 21:20:57 +0530

    while loop

--- src/loopsExample.java
+++ src/loopsExample.java
@@ -2,27 +2,19 @@ class loopsExample {
 
     public static void main(String args[]){
 
-        //For Loop increment 1
-        for(int i=0;i<=10;i++){
-            System.out.print(i+ " ");
-        }
-            System.out.println();
-
-        //for Loop increment 2
-        for(int j=0;j<=10;j=j+2){
-            System.out.print(j+" ");
-        }
-            System.out.println();
-
-        //for Loop Decrement
-        for(int k=10;k>=1;k--){
-            System.out.print(k+" ");
+        //increment in while loop
+        int i=1;
+        while(i<=10){
+            System.out.print(i+" ");
+            i++;
         }
         System.out.println();
 
-        //For decrement 2
-        for(int p=10; p>=0; p=p-2){
-            System.out.print(p+ " ");
+        //Decrement in while loop
+        int j=10;
+        while(j>=1){
+            System.out.print(j+" ");
+            j--;
         }
 
     }
