feb7cf406d08b1fd110395519cc6f9553f84b5af
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-23 21:47:48 +0530

    Break Statement

---
+++ src/BreakContinueStatement.java
@@ -0,0 +1,13 @@
+public class BreakContinueStatement {
+
+    public static void main(String args[]){
+
+        //break Statement
+        for(int i=1;i<=100;i++){
+            System.out.print(i+" ");
+            if(i==50){
+                break;
+            }
+        }
+    }
+}
