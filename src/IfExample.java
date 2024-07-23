2cd90826eb482774f1d7595a6f91b7d5626bfff8
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-23 15:15:50 +0530

    If else statement

--- src/IfExample.java
+++ src/IfExample.java
@@ -1,11 +1,12 @@
 class IfExample {
 
     public static void main(String arg[]){
-        int n = 10;
+        int n = -10;
         if(n>0){
             //If body: if this condition true then print this statement
            System.out.println("Yes it is grater then " +n);
-
+        } else {
+            System.out.println("No it is not grater then " +n);
         }
     }
 }
