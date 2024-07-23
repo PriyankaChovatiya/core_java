8e3a44234008a037627ac927cec06c01922c808c
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-23 14:04:15 +0530

    Variables

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,16 @@ class Test {
     public static void main(String[] args){
         System.out.print("Hello");
         System.out.println(" world");
+
+        int a=10;
+        String name = "Priyaa";
+        System.out.println(a);
+        System.out.println(name);
+        a = 20;
+        name = "Mastermind";
+        System.out.println(a);
+        System.out.println(name);
+
+
     }
 }
