9c75774336727e6333221a30754d7050bb0ecf2a
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-24 14:54:47 +0530

    Inheritance (Files Animal,Dog,Demo)

--- src/oops/Demo.java
+++ src/oops/Demo.java
@@ -3,25 +3,10 @@ package oops;
 public class Demo {
     public static void main(String args[]){
 
-        //Now create Object
-        Student st1,st2;
-        st1 = new Student();
-
-        st1.StudentId   = 10;
-        st1.StudentName = "Priyaa";
-        st1.StudentCity = "Guj";
-        st1.Study();
-        st1.StudentFullDetails();
-
-            System.out.println();
-
-        st2 = new Student(1020, "Priyaa M", "Banglore");
-
-//        st2.StudentId   = 20;
-//        st2.StudentName = "MasterMind";
-//        st2.StudentCity = "Raj";
-        st2.Study();
-        st2.StudentFullDetails();
+        //create object
+       Dog d1 = new Dog();
+       d1.eating();
+       System.out.println(d1.color);
 
 
     }
