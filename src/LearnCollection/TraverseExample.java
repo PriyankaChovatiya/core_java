ed4a04d4ff2a6c8cfe9d0ba9cdb1ef9d693d26d5
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-25 22:22:17 +0530

    Methods : forward,backward,foreach,sorting (Files: TraverseExample)

--- src/LearnCollection/TraverseExample.java
+++ src/LearnCollection/TraverseExample.java
@@ -1,6 +1,10 @@
 package LearnCollection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.TreeSet;
+import java.util.function.Consumer;
 
 public class TraverseExample {
 
@@ -10,6 +14,7 @@ public class TraverseExample {
         names.add("xyz");
         names.add("Tina");
 
+        System.out.println("_______REVERSE STRING_________");
         //for each loop
         for(String str:names){
             System.out.print(str+ "\t"+str.length()+ "\t");
@@ -17,7 +22,40 @@ public class TraverseExample {
             System.out.println(br.reverse());
         }
 
-        System.out.println(names);
+        System.out.println("___________Forward Traversing_________________");
+
+        //Traversing Using ITERATOR : FORWARD Traversing
+        Iterator<String> itr = names.iterator();
+        {
+            while (itr.hasNext()){
+                String next = itr.next();
+                System.out.println(next);
+            }
+
+        }
+        System.out.println("________Backward Traversing______________________");
+        // Backward Traversing : ListIteration
+        ListIterator<String> litr = names.listIterator(names.size());
+          while (litr.hasPrevious()){
+              String previous= litr.previous();
+              System.out.println(previous);
+          }
+
+          System.out.println("___________FOR EACH METHOD_____________");
+
+          //for each method
+         names.forEach(e->{
+             System.out.println(e);
+         });
+
+         System.out.println("_______SORTING ELEMENT__________");
+
+        TreeSet<String> set = new TreeSet<>();
+        set.addAll(names);
+        set.forEach(e ->{
+            System.out.println(e);
+        });
+
     }
 
 }
